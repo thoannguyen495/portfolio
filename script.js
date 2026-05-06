10a76e4576950d33692063bef1b4c6cd45e5b926
thoannguyen495 <lc23-0561@lclark.edu>
2026-05-06 16:36:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const learnMoreButtons=document.querySelectorAll.(".learn-more-btn");
 
+learnMoreButtons.forEach((button)=>{
+  button.addEventListener("click",()=>{
+
+    const extraInfo = button.nextElementSibling;
+
+    if (extraInfo.style.display=="block"){
+      extraInfo.style.display="none";
+      button.textContent="LearnMore";
+    }
+
+    else {
+      extraInfo.style.display="block";
+      button.textContent="Show Less";
+    }
+  });
+});
+
+const darkModeButton = document.getElementById("dark-mode-btn");
+
+darkModeButton.addEventListener("click",()=>{
+  document.body.classList.toggle("dark-mode");});
+
+const form=document.querySelector("form");
+
+form.addEventListener("submit", (event) =>
+  {
+    const name = document.querySelector('input[name="name"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const email = document.querySelector('textarea[name="message"]').value;
+
+    if (name===""||email===""|message===""){
+      event.preventDefault();
+      alert("Please fill in all fields.");}});
+
+
+      
